03d7ac4e6714699f8709a432e7a2561018f7c330
zuniversal <604688489@qq.com>
2022-11-23 17:29:53 +0800

    edit 修改组件入口

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 
-export { default as SmartForm } from './SmartForm/index.jsx'// 
+// export { default as SmartForm } from './SmartForm/index.jsx'// 
+import SmartForm from './SmartForm/index.jsx'// 
+
+export default {
+  SmartForm,
+} // 
